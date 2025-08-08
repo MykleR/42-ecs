0d5c0ccc7faed4409ca2ac601d5d9788cff0ad8c
MykleR <michael.rouves@gmail.com>
2025-08-08 16:58:54 +0200

    🚧 wip: WIP: Fix#1 Merged Header-only V2, re added headers/utils.h

---
+++ headers/utils.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 19:03:10 by mrouves           #+#    #+#             */
+/*   Updated: 2025/08/08 16:57:03 by mrouves          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <stdlib.h>
+
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size);
+
+#endif
