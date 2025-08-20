04d953e193cf267304f85c374a0a6cccd9ee28d7
MykleR <michael.rouves@gmail.com>
2025-08-20 13:39:17 +0200

    🚧 wip: imported utest.h to run tests

--- v2-header-only/unittests/unittest.c
+++ v2-header-only/unittests/unittest.c
@@ -1,9 +1,36 @@
+#include <utest.h>
 #include <ecs_utils.h>
 
-int main()
-{
-	// This is a placeholder for the main function.
-	// You can add your test cases here.
-	LOG_OK("Unit tests started");
-	return 0;
+
+__attribute__((constructor))
+void init() { LOG_INFO("STARTING\n");}
+
+UTEST(ecs_vec, init) {
+	UTEST_SKIP("Need to implement this test!");
 }
+
+UTEST(ecs_vec, push) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST(ecs_vec, pop) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST(ecs_vec, resize) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST(ecs_vec, clear) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST(ecs_vec, free) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST(ecs_vec, iter) {
+	UTEST_SKIP("Need to implement this test!");
+}
+
+UTEST_MAIN();
